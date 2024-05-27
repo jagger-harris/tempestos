321de33621e2ea5d199fb303f06c4b1737a34610
jagger-harris <jagger.lee.harris@gmail.com>
2024-05-27 17:32:49 -0500

    Initial commit

---
+++ .github/README.md
@@ -0,0 +1,27 @@
+# TempestOS
+
+     _____                              _   _____ _____ 
+    |_   _|                            | | |  _  /  ___|
+      | | ___ _ __ ___  _ __   ___  ___| |_| | | \ `--. 
+      | |/ _ \ '_ ` _ \| '_ \ / _ \/ __| __| | | |`--. \
+      | |  __/ | | | | | |_) |  __/\__ \ |_\ \_/ /\__/ /
+      \_/\___|_| |_| |_| .__/ \___||___/\__|\___/\____/ 
+                       | |                              
+                       |_|                              
+
+TempestOS is my personal Arch Linux post-installation script. It will install the majority of the packages I use as well as my dotfiles.
+
+__**WARNING - THIS CAN OVERWRITE YOUR DOTFILES IF YOU ARE NOT CAREFUL!**__
+
+## Using and running
+```shell
+git clone https://github.com/jagger-harris/tempestos.git
+chmod +x ./tempestos.sh
+./tempestos.sh
+```
+
+## Contributing
+Bug fix contributions only.
+
+## License
+- [ISC](https://choosealicense.com/licenses/isc/)
---
+++ tempestos.sh
@@ -0,0 +1,153 @@
+#!/bin/sh
+
+#   _____                              _   _____ _____
+#  |_   _|                            | | |  _  /  ___|
+#    | | ___ _ __ ___  _ __   ___  ___| |_| | | \ `--.
+#    | |/ _ \ '_ ` _ \| '_ \ / _ \/ __| __| | | |`--. \
+#    | |  __/ | | | | | |_) |  __/\__ \ |_\ \_/ /\__/ /
+#    \_/\___|_| |_| |_| .__/ \___||___/\__|\___/\____/
+#                     | |
+#                     |_|
+#
+# TempestOS Arch Linux post-install script
+# RUN THIS AT YOUR OWN RISK
+# SCRIPT IS SAFE, BUT WILL OVERWRITE YOUR DOTFILES
+
+# Functions
+#################################################
+
+confirm_prompt() {
+  prompt="$1"
+  while true; do
+    echo "$prompt (yes/no)"
+    read -r answer
+    case "$answer" in
+    [Yy] | [Yy][Ee][Ss]) return 1 ;;
+    [Nn] | [Nn][Oo]) return 0 ;;
+    *) echo "Invalid input. Please enter 'yes' or 'no'." ;;
+    esac
+  done
+}
+
+install_official_packages() {
+  official_packages_file="official_packages.txt"
+  
+  if [ -f "$official_packages_file" ]; then
+    confirm_prompt "Install official Arch packages?"
+    if [ $? -eq 1 ]; then
+      echo "Installing official packages..."
+      while IFS= read -r package || [ -n "$package" ]; do
+        sudo pacman -S --needed --noconfirm "$package"
+      done <"$official_packages_file"
+    else
+      echo "Skipping official packages..."
+    fi
+  else
+    echo "$official_packages_file not found"
+    confirm_prompt "Continue installation?"
+    [ $? -eq 1 ] || exit 0
+  fi
+}
+
+install_aur_packages() {
+  aur_packages_file="aur_packages.txt"
+
+  if [ -f "$aur_packages_file" ]; then
+    confirm_prompt "Install AUR packages?"
+    if [ $? -eq 1 ]; then
+      echo "Installing AUR packages..."
+      if ! command -v paru >/dev/null 2>&1; then
+        echo "Installing paru AUR helper..."
+        git clone https://aur.archlinux.org/paru-bin.git
+        (
+          cd paru-bin || exit
+          makepkg -si --noconfirm
+          cd ..
+          rm -rf paru-bin
+        )
+      fi
+      while IFS= read -r package || [ -n "$package" ]; do
+        paru -S --needed --noconfirm "$package"
+      done <"$aur_packages_file"
+    else
+      echo "Skipping AUR packages..."
+    fi
+  else
+    echo "$aur_packages_file not found"
+    confirm_prompt "Continue installation?"
+    [ $? -eq 1 ] || exit 0
+  fi
+}
+
+install_dotfiles() {
+  echo "*** ANSWERING YES WILL OVERWRITE YOUR CURRENT DOTFILES! ***"
+  echo "*** Make sure to back them up before continuing. ***"
+  echo
+
+  confirm_prompt "Install dotfiles?"
+  if [ $? -eq 1 ]; then
+    echo "Cloning dotfiles git repo to temp folder..."
+    git clone https://github.com/jagger-harris/dotfiles.git dotfiles_tmp
+    echo "Copying dotfiles to home directory..."
+    cp -a dotfiles_tmp/. "$HOME"
+    echo "Removing temp folder"
+    rm -rf dotfiles_tmp
+  else
+    echo "Skipping dotfiles..."
+  fi
+}
+
+# Main script
+#################################################
+
+cat logo.txt
+echo
+echo "Welcome to the TempestOS Arch post-installation script!"
+echo
+
+if [ "$(id -u)" = 0 ]; then
+  echo "Detected installation script running as the root user."
+  echo "DO NOT RUN THE SCRIPT AS THE ROOT USER!"
+  exit 0
+fi
+
+echo "Follow instructions carefully. There are options to not install packages."
+echo "However, it is recommended to answer (y/yes) to all of them."
+echo
+echo "Press ENTER to continue..."
+head -n 1 >/dev/null
+
+# Update system and install necessary packages
+echo "0. Update system and install required packages"
+echo "----------------------------"
+echo
+echo "Updating system and installing necessary packages..."
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel git
+
+# Install official packages
+echo
+echo "1. Install official packages"
+echo "----------------------------"
+install_official_packages
+
+# Install AUR packages
+echo
+echo "2. Install AUR packages"
+echo "----------------------------"
+echo
+install_aur_packages
+
+# Install dotfiles
+echo
+echo "3. Clone TempestOS dotfiles"
+echo "----------------------------"
+echo
+install_dotfiles
+
+# Completion
+echo
+echo "4. Completion"
+echo "----------------------------"
+echo
+echo "TempestOS Arch post-installation script is completed!"
